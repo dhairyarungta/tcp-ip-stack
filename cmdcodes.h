66fac86767bafe1b8b0433877f537e2fb0726469
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2024-04-17 11:14:09 +0800

    Add feature to Cli

--- cmdcodes.h
+++ cmdcodes.h
@@ -1,2 +1,3 @@
 /*cli integration helper file*/
 #define CMDCODE_SHOW_NW_TOPOLOGY 1
+#define CMDCODE_RUN_ARP_RESOLUTION_IP_ADDR 2
--- nwcli.c
+++ nwcli.c
@@ -8,6 +8,38 @@
 
 extern graph_t *topo;
 
+static int
+validate_node_name (char *value){
+    assert(strnlen(value,NODE_NAME_SIZE)<=NODE_NAME_SIZE);
+    node_t *node = get_node_by_node_name(topo,value);
+    if(node!=NULL)
+        return VALIDATION_SUCCESS;
+    else 
+        return VALIDATION_FAILED;
+}
+
+static int
+arp_handler (param_t *param, ser_buff_t *tlv_buf,
+    op_mode enable_or_disable){
+        int cmd_code = -1;
+        cmd_code = EXTRACT_CMD_CODE(tlv_buf);
+	//printf("cmd_code = %d\n", cmd_code);
+        char *ip_addr = NULL;
+        char *node_name = NULL;
+        tlv_struct_t *tlvptr = NULL ; 
+
+        TLV_LOOP_BEGIN(tlv_buf, tlvptr){
+            if(strncmp(tlvptr->leaf_id, "ip-address", strlen("ip-address"))==0){
+                ip_addr = tlvptr->value;
+            }
+            else if(strncmp(tlvptr->leaf_id, "node-name", strlen("node-name"))==0){
+                node_name = tlvptr->value;
+            }
+        }TLV_LOOP_END;
+        printf("Node Name : %s, IP Address : %s", node_name,ip_addr);
+        return 0;
+
+}
 
 static int
 show_nw_topology_handler(param_t *param, ser_buff_t *tlv_buf, 
@@ -49,6 +81,31 @@ nw_init_cli(){
 
         //}
 
+    }
+
+    {
+        static param_t node;
+        init_param(&node, CMD, "node",0,0,INVALID,0,"Run Instructions on a Node");
+        libcli_register_param(run,&node);
+
+        {
+            static param_t node_name;
+            init_param(&node_name, LEAF,0, 0, validate_node_name,STRING, "node-name","Node Name Present in Topology");
+            libcli_register_param(&node, &node_name);
+
+            {
+                static param_t resolve_arp;
+                init_param(&resolve_arp, CMD, "resolve-arp",0,0,INVALID,0,"Reslove Address Using ARP");
+                libcli_register_param(&node_name,&resolve_arp);
+
+                {
+                    static param_t ip_addr;
+                    init_param(&ip_addr, LEAF, 0, arp_handler, 0,IPV4,"ip-address","Run Resolve ARP on IP Address");
+                    libcli_register_param(&resolve_arp,&ip_addr);
+                    set_param_cmd_code(&ip_addr, CMDCODE_RUN_ARP_RESOLUTION_IP_ADDR);
+                }
+            }
+        }
     }
     support_cmd_negation(config);
 }
